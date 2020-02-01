5ae735f7c508438e4d7206b020e3c1ab39772c52
Mihàly Verhàs <verhasm@Mihalys-MacBook-Pro.local>
2020-02-01 16:34:52 +0100

    Galaxy class expanded.

--- aether-domain/src/main/java/com/aether/domain/celestials/Galaxy.java
+++ aether-domain/src/main/java/com/aether/domain/celestials/Galaxy.java
@@ -1,7 +1,33 @@
 package com.aether.domain.celestials;
 
+import com.aether.model.celestials.CelestialBody;
+import com.aether.model.celestials.CelestialObject;
+
+import java.util.List;
+
 /**
- * Represents a galaxy, with it's systems.
+ * Represents a galaxy, with its systems.
  */
-public class Galaxy {
+public class Galaxy implements CelestialObject {
+    List<CelestialObject> nebulae;
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public List<CelestialObject> getSubstructures() {
+        return null;
+    }
+
+    @Override
+    public List<CelestialBody> getBodies() {
+        return null;
+    }
 }
